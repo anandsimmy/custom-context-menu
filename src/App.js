9493378fd5456464ebc7567b07985f63d1d81788
Anand Simmy <anandsimmy7@gmail.com>
2021-07-14 08:34:37 +0530

    added context menu grids

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import MyCustomContextMenu from './MyCusomtContextMenu';
 
-function App() {
+const App= () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='wrapper'>
+      <div id='defaultContextmenuArea' className='defaultContextmenuArea' />
+      <div id='customContextmenuArea1' className='customContextmenuArea1' />
+      <div id='customContextmenuArea2' className='customContextmenuArea2' />
+      <div id='customContextmenuArea3' className='customContextmenuArea3' />
+      <MyCustomContextMenu />
     </div>
   );
 }
