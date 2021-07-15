f233e8ca56579f2424599e8e674d94a6ea0c9fe3
Anand Simmy <anandsimmy7@gmail.com>
2021-07-15 09:00:11 +0530

    added custom context menu

--- src/App.js
+++ src/App.js
@@ -4,11 +4,26 @@ const App= () => {
 
   return (
     <div className='wrapper'>
-      <div id='defaultContextmenuArea' className='defaultContextmenuArea' />
-      <div id='customContextmenuArea1' className='customContextmenuArea1' />
-      <div id='customContextmenuArea2' className='customContextmenuArea2' />
-      <div id='customContextmenuArea3' className='customContextmenuArea3' />
-      <MyCustomContextMenu />
+      <div id='defaultContextmenuArea' className='defaultContextmenuArea'>
+      </div>
+      <div id='customContextmenuArea1' className='customContextmenuArea1' >
+        <MyCustomContextMenu
+          targetId='customContextmenuArea1'
+          options={['View', 'Update', 'Delete']}
+        />
+      </div>
+      <div id='customContextmenuArea2' className='customContextmenuArea2' >
+        <MyCustomContextMenu
+          targetId='customContextmenuArea2'
+          options={['Netflix', 'Prime', 'Hotstar', 'Youtube']}
+        />
+      </div>
+      <div id='customContextmenuArea3' className='customContextmenuArea3' >
+        <MyCustomContextMenu
+          targetId='customContextmenuArea3'
+          options={['Footbal', 'Cricket', 'BasketBall', 'Volleyball', 'Tennis', 'Hockey', 'Golf']}
+        />
+      </div>
     </div>
   );
 }
