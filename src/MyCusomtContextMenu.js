d40bff723836cc817bb3dbc98510ca29b0af94f6
Anand Simmy <anandsimmy7@gmail.com>
2021-07-15 09:00:25 +0530

    added offclick handler

--- src/MyCusomtContextMenu.js
+++ src/MyCusomtContextMenu.js
@@ -1,22 +1,44 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import './App.css';
 
-const MyCustomContextMenu= () => {
+const MyCustomContextMenu= ({ targetId, options }) => {
   const [contextData, setContextData]= useState({ visible:false, posX: 0, posY: 0});
+  const contextRef= useRef(null);;
 
   useEffect(() => {
     const contextMenuEventHandler= (event) => {
-      event.preventDefault();
-      setContextData({ posX: event.clientX, posY: event.clientY })
+      const targetElement= document.getElementById(targetId)
+      if(targetElement && targetElement.contains(event.target)){
+        event.preventDefault();
+        setContextData({ visible: true, posX: event.clientX, posY: event.clientY })
+      }else if(contextRef.current && !contextRef.current.contains(event.target)){
+        setContextData({ ...contextData, visible: false })
+      }
+    }
+
+    const offClickHandler= (event) => {
+      if(contextRef.current && !contextRef.current.contains(event.target)){
+        setContextData({ ...contextData, visible: false })
+      }
     }
 
     document.addEventListener('contextmenu', contextMenuEventHandler)
-    return () => document.removeEventListener('contextmenu', contextMenuEventHandler)
-  }, [])
+    document.addEventListener('click', offClickHandler)
+    return () => {
+      document.removeEventListener('contextmenu', contextMenuEventHandler)
+      document.removeEventListener('click', offClickHandler)
+    }
+  }, [contextData, targetId])
 
   return (
-    <div className='contextMenu' style={{ left: contextData.posX, top: contextData.posY }}>
-      Hello, Anand is the best
+    <div ref={contextRef} className='contextMenu' style={{ display:`${contextData.visible ? 'block' : 'none'}`, left: contextData.posX, top: contextData.posY }}>
+      <div className='optionsList'>
+        {options.map((option) => (
+          <li key={option} className='optionListItem'>
+            {option}
+          </li>
+        ))}
+      </div>
     </div>
   );
 }
