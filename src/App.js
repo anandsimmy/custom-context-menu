19d84fcda777b529dfeffe82a2d154015d75d25e
Anand Simmy <anandsimmy7@gmail.com>
2021-07-31 10:00:22 +0530

    context menu changes

--- src/App.js
+++ src/App.js
@@ -10,18 +10,30 @@ const App= () => {
         <MyCustomContextMenu
           targetId='customContextmenuArea1'
           options={['View', 'Update', 'Delete']}
+          classes={{
+            listWrapper: 'customContextmenuArea1ListWrapper',
+            listItem: 'customContextmenuArea1ListItem'
+          }}
         />
       </div>
       <div id='customContextmenuArea2' className='customContextmenuArea2' >
         <MyCustomContextMenu
           targetId='customContextmenuArea2'
           options={['Netflix', 'Prime', 'Hotstar', 'Youtube']}
+          classes={{
+            listWrapper: 'customContextmenuArea2ListWrapper',
+            listItem: 'customContextmenuArea2ListItem'
+          }}
         />
       </div>
       <div id='customContextmenuArea3' className='customContextmenuArea3' >
         <MyCustomContextMenu
           targetId='customContextmenuArea3'
           options={['Footbal', 'Cricket', 'BasketBall', 'Volleyball', 'Tennis', 'Hockey', 'Golf']}
+          classes={{
+            listWrapper: 'customContextmenuArea3ListWrapper',
+            listItem: 'customContextmenuArea3ListItem'
+          }}
         />
       </div>
     </div>
